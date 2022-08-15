372a60633f0ee5a73c268b7e4c2e0de414d008af
Fillipe Meireles <hebriquefillipe@gmail.com>
2022-08-15 17:57:18 -0300

    (doing) checkin arr[i] with arr[j]

---
+++ main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+	int arr[] = {5,1,3,6,4,7,2};
+	for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
+		for(int j = i; j < sizeof(arr)/sizeof(arr[0]); j++)
+		{
+			int smaller_n_index;
+			int smaller;
+			if(arr[i] > arr[j])
+
+		}
+		printf("\n");
+	}
+}
