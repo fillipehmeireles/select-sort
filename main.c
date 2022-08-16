5dc080d549ff9409ebf80514ec374ec23cfba5e8
Fillipe Meireles <hebriquefillipe@gmail.com>
2022-08-16 08:01:25 -0300

    Select Sort

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
 
 int main() {
-	int arr[] = {5,1,3,6,4,7,2};
-	for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
-		for(int j = i; j < sizeof(arr)/sizeof(arr[0]); j++)
-		{
-			int smaller_n_index;
-			int smaller;
-			if(arr[i] > arr[j])
-
-		}
-		printf("\n");
-	}
+    int arr[] = {5,1,3,6,4,7,2};
+    for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
+        int smaller = arr[i];
+        int smaller_n_index = i;
+        for(int j = i; j < sizeof(arr)/sizeof(arr[0]); j++)
+        {
+            if(smaller > arr[j])
+            {
+                smaller_n_index = j;
+                smaller = arr[j];
+            }
+        }
+        arr[smaller_n_index] = arr[i];
+        arr[i] = smaller;
+    }
+    for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
+        printf("%d\n", arr[i]);
+    return 0;
 }
